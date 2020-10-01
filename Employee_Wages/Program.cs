bb412352371189438dde3980ef2836e29db698dd
Priya Biriya <priyabiriya1707@gmail.com>
2020-10-01 15:10:13 +0530

    [Priya] add . Wage For Multiple companies

--- Employee_Wages/Program.cs
+++ Employee_Wages/Program.cs
@@ -6,7 +6,7 @@ namespace Employee_Wages
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World...!");
         }
     }
 }
